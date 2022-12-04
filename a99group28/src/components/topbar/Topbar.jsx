015e292ba38ef63d42792a04952d040007a58c81
Andrew Knotts <knottsan2010@gmail.com>
2022-12-04 13:41:18 -0500

    continuing to build top bar

--- a99group28/src/components/topbar/Topbar.jsx
+++ a99group28/src/components/topbar/Topbar.jsx
@@ -1,7 +1,9 @@
 
 import "./topbar.css";
-import {Search} from "@material-ui/icons";
-import {Person} from "@material-ui/icons";
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Topbar(){
    return( 
@@ -12,7 +14,7 @@ export default function Topbar(){
         </div>
         <div className="topbarCenter">
             <div className="searchbar">
-               <Search/>
+               <SearchIcon/>
                <input placeholder="Search for friend or post" className="searchInput" />
             </div>
         </div>
@@ -23,11 +25,20 @@ export default function Topbar(){
             </div>
       <div className="topbarIcons">
          <div className="topbarIconItem">
-            {/* <Person/> */}
+            <PersonIcon/>  
+            <span className="topbarIconBadge">1</span>
+         </div>
+         <div className="topbarIconItem">
+            <ChatIcon/>  
+            <span className="topbarIconBadge">2</span>
+         </div>
+         <div className="topbarIconItem">
+            <NotificationsIcon/>  
             <span className="topbarIconBadge">1</span>
          </div>
       </div>
-        </div>
+      <img src="/assets/people/patrick2.jpeg" alt="" className="topbarImg" />
+    </div>
 
     </div>
    )
