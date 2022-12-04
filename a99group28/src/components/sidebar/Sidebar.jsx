96e192677ecc91eaa0e67138ca72da96c358c9f2
Andrew Knotts <knottsan2010@gmail.com>
2022-12-04 15:08:50 -0500

    added side bar, changed styles

---
+++ a99group28/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,47 @@
+import "./sidebar.css";
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import GroupIcon from '@mui/icons-material/Group';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+import { Users } from "../../dummyData";
+import CloseFriend from "../closeFriend/CloseFriend";
+import ChatIcon from '@mui/icons-material/Chat';
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <ChatIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Chats</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircleFilledIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+            <GroupIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <BookmarkIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Bookmarks</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          {Users.map((u) => (
+            <CloseFriend key={u.id} user={u} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- a99group28/src/components/topbar/Topbar.jsx
+++ a99group28/src/components/topbar/Topbar.jsx
@@ -10,18 +10,17 @@ export default function Topbar(){
    <div className="topbarContainer">
 
         <div className="topbarLeft">
-            <span className="logo">Nomen</span>
+            <span className="logo">Nomenconnect</span>
         </div>
         <div className="topbarCenter">
             <div className="searchbar">
-               <SearchIcon/>
+               <SearchIcon className="searchIcon"/>
                <input placeholder="Search for friend or post" className="searchInput" />
             </div>
         </div>
         <div className="topbarRight">
             <div className="topbarLinks">
                <span className="topbarLink">Homepage</span>
-               <span className="topbarLink">Timeline</span>
             </div>
       <div className="topbarIcons">
          <div className="topbarIconItem">
