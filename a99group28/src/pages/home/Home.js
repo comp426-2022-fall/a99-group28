f3f2da95a56c153a2d750a48059f6b188653d79e
Davis Smith <44070645+davissmith1@users.noreply.github.com>
2022-12-08 16:50:50 -0500

    api working

--- a99group28/src/pages/home/Home.js
+++ a99group28/src/pages/home/Home.js
@@ -20,6 +20,8 @@ export default function Home(){
         console.log(first_name + last_name + email + password);
 
     }
+    
+    var sname;
 
     const adduser = () =>{
         Axios.post('http://localhost:3001/adduser', {
@@ -33,22 +35,28 @@ export default function Home(){
     };
 
     const getUsers = () =>{
-        Axios.get('http://localhost:3001/getnames').then((response) => {
+        Axios.get('http://cors-anywhere.localhost:3001/getnames').then((response) => {
             getNames(response.data);
             console.log("Users Fecthed")
+            console.log(f_name);
+            
         });
     };
+
 
     const options = {
     method: 'GET',
     url: 'https://love-calculator.p.rapidapi.com/getPercentage',
-    params: {fname: 'John', sname: 'Alice'},
-    
+    params: {fname:f_name, sname: 'Alice'},
+    headers: {
+        'X-RapidAPI-Key': '703558ec3bmshef7ac65c502cce8p1f3ac8jsn349957f27b6d',
+        'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
+      }
     };
     
     const getPercentage = () =>{
         Axios.request(options).then(function (response) {
-            console.log(response.data);
+            console.log(response.data.percentage);
         }).catch(function (error) {
             console.error(error);
         });
@@ -91,6 +99,9 @@ export default function Home(){
                   }}
                 />
 
+                <button onClick={adduser}>Add User</button>
+                
+
                 <label>First Name For Compatibility Test</label>
                 <input type = "text"
                 onChange={(event) =>{
@@ -98,14 +109,11 @@ export default function Home(){
                 }}
                 />
 
-                <button onClick={adduser}>Add User</button>
-                <button onClick={getPercentage}> Get Compatibility Test</button>
-            
-            
-            </div>
-            <div className="users">
                 <button onClick={getUsers}>Get Compatibility</button>
                
+            </div>
+            <div className="users">
+                
             </div>
          
         <Sidebar/>
