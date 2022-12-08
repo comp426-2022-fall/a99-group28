ce1e05481529fd330f6b91c2172318c5a969e57d
Davis Smith <44070645+davissmith1@users.noreply.github.com>
2022-12-07 20:57:10 -0500

    sql code running

--- a99group28/serverStuff/sql.js
+++ a99group28/serverStuff/sql.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const mysql = require("mysql")
+var bodyParser = require('body-parser')
+
 
 //create connection
 const db = mysql.createConnection({
@@ -23,16 +25,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) =>{
-    sql = "USE userData"
-    db.query(sql, (err) =>{
-        if(err){
-            throw err;
-        }
-        res.send("Database conected")
-        console.log("database running")
-    });
-});
 
 //creates the data base
 app.get("/createdb", (req, res) =>{
@@ -46,15 +38,27 @@ app.get("/createdb", (req, res) =>{
     });
 });
 
+//connects database
+app.get("/", (req, res) =>{
+    sql = "USE userData"
+    db.query(sql, (err) =>{
+        if(err){
+            throw err;
+        }
+        res.send("Database conected")
+        console.log("database running")
+    });
+});
 
 
+//creates user table
 app.get("/createtable", (req, res) =>{
     let sql = `CREATE TABLE users (
         user_id int AUTO_INCREMENT, 
         first_name VARCHAR(255), 
         last_name VARCHAR(255), 
         email VARCHAR(255), 
-        bday DATE, 
+        password VARCHAR(255), 
         PRIMARY KEY(user_id));`
 
     db.query(sql, (err) =>{
@@ -65,15 +69,17 @@ app.get("/createtable", (req, res) =>{
     });
 });
 
+//tests for post request
 app.post("/posttest", (req, res) =>{
-        let data = JSON.stringify(req.body);
-        res.send(data)
-        console.log(data + " Working ");
+        let data = req.body;
+        console.log(data);
+        console.log(data.test + " Working \n");
 });
 
+//add user
 app.post("/adduser", (req, res) =>{
     var data = req.body;
-    let post = { first_name: data.first_name, last_name: data.last_name, email: data.email, bday: data.bday};
+    let post = { first_name: data.first_name, last_name: data.last_name, email: data.email, password: data.password};
 
     console.log(data.first_name);
 
@@ -84,27 +90,97 @@ app.post("/adduser", (req, res) =>{
             throw err;
         }
 
-        res.send("User added");
+        res.send("User added\n");
     });
 });
 
-app.get("/addusertest", (req, res) =>{
-    var data = req.body;
-    let post = { first_name: "Dave", last_name: "Smith", email: "dav.smith@email.com", bday:'2000-1-1'};
+//update user first name
+app.post("/updatefirstanme:id", (req, res) =>{
+    let data = req.body;
+    let id = req.params.id;
+    let first_name = data.first_name;
 
-    let sql = "INSERT INTO users SET ?";
+    let sql = 'UPDATE user SET first_name = ' + first_name + ' WHERE user_id =' + id;
 
-    let query = db.query(sql, post, (err) =>{
+    let query = db.query(sql, (err) => {
         if(err){
             throw err;
         }
 
-        res.send("User");
+        res.send("User first name update");
     });
 });
 
+//update user last name
+app.post("/updatelastname:id", (req, res) =>{
+    let data = req.body;
+    let id = req.params.id;
+    let last_name = data.last_name;
+
+    let sql = 'UPDATE user SET last_name = ' + last_name + ' WHERE user_id =' + id;
+
+    let query = db.query(sql, (err) => {
+        if(err){
+            throw err;
+        }
+
+        res.send("User last name update");
+    });
+});
+
+//update user email
+app.post("/updateemail:id", (req, res) =>{
+    let data = req.body;
+    let id = req.params.id;
+    let last_name = data.email;
+
+    let sql = 'UPDATE user SET email= ' + email + ' WHERE user_id =' + id;
+
+    let query = db.query(sql, (err) => {
+        if(err){
+            throw err;
+        }
+
+        res.send("User email update");
+    });
+});
+
+//update user password
+app.post("/updatepassword:id", (req, res) =>{
+    let data = req.body;
+    let id = req.params.id;
+    let last_name = data.password;
+
+    let sql = 'UPDATE user SET password= ' + password + ' WHERE user_id =' + id;
+
+    let query = db.query(sql, (err) => {
+        if(err){
+            throw err;
+        }
+
+        res.send("User password update");
+    });
+
+});
+
+//delet user
+app.post("/deleteuser:id", (req, res) =>{
+    let id = req.params.id;
+
+    let sql = "DELETE FROM user WHERE user_id =" + id;
+
+    let query= db.query(sql, (err) =>{
+        if(err){
+            throw err;
+        }
+
+        res.send("User Deleted");
+    });
+});
+
+//pull all first and last names
 app.get("/getnames/", (req, res) =>{
-    let sql = "SELECT firs_anme FROM users";  
+    let sql = "SELECT first_name, last_name FROM users";  
     
     let query = db.query(sql, function(err, results, feilds) {
         if(err){
@@ -116,7 +192,7 @@ app.get("/getnames/", (req, res) =>{
     });
 });
 
-
+//pull a user based on id
 app.get("/getuser/:id", (req, res) =>{
     let sql = "SELECT * FROM users WHERE user_id =" + req.params.id;  
     
@@ -130,10 +206,7 @@ app.get("/getuser/:id", (req, res) =>{
     });
 });
 
-
-
-
-
+//start the app
 app.listen("3000", () =>{
     console.log("Server started on port 3000");
 });
